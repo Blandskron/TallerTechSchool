6e6e1beeeee1f8efd80a3529eee290868eeaac25
Blandskron <bastianlandskronfreelancer@gmail.com>
2023-10-28 15:17:18 -0300

    addmore

--- pokeapi/pokeapi/src/App.jsx
+++ pokeapi/pokeapi/src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/?limit=151")
+      .then((data) => data.json())
+      .then((data) => setPokemons(data.results));
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>PokeDex</h1>
+      <ul>
+        {pokemons.map((pokemon, index) => (
+          <li key={pokemon.name}>
+            {pokemon.name}
+            <LazyLoadImage
+              alt={pokemon.name}
+              height="96px"
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+                index + 1
+              }.png`}
+              width="96px"
+            />
+          </li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
